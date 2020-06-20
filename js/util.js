d45a2f8c65610dcaf64bd968b615124a00bf7329
0mbmb <as.klementev@gmail.com>
2020-06-20 13:23:05 +0300

    Правки в util.js

--- js/util.js
+++ js/util.js
@@ -2,31 +2,34 @@
 
 (function () {
 
-  window.util = {
-    getRandomFromArray: function (array) {
-      return array[Math.floor(Math.random() * array.length)];
-    },
-    getRandomIntFromRange: function (min, max) {
-      return Math.floor(Math.random() * (max - min + 1) + min);
-    },
-    removeRandomItemsFromArray: function (array) {
-      var newArray = [];
-      for (var i = 0; i < array.length; i++) {
-        if (window.util.getRandomIntFromRange(0, 1)) {
-          newArray.push(array[i]);
-        }
-      }
-      return newArray;
-    },
-    isMapActive: function () {
-      var mapContainer = document.querySelector('.map');
-      return !mapContainer.classList.contains('map--faded');
-    },
-    onEnterClick: function (evt, executeFunction) {
-      if (evt.key === 'Enter') {
-        executeFunction();
+  function getRandomFromArray(array) {
+    return array[Math.floor(Math.random() * array.length)];
+  }
+
+  function getRandomIntFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  function removeRandomItemsFromArray(array) {
+    var newArray = [];
+    for (var i = 0; i < array.length; i++) {
+      if (window.util.getRandomIntFromRange(0, 1)) {
+        newArray.push(array[i]);
       }
     }
+    return newArray;
+  }
+
+  function isMapActive() {
+    var mapContainer = document.querySelector('.map');
+    return !mapContainer.classList.contains('map--faded');
+  }
+
+  window.util = {
+    getRandomFromArray: getRandomFromArray,
+    getRandomIntFromRange: getRandomIntFromRange,
+    removeRandomItemsFromArray: removeRandomItemsFromArray,
+    isMapActive: isMapActive
   };
 
 })();
